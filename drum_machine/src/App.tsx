23cc26f935c8ce488d0096d4c3f27f74756767fa
mlshere <mlsherezade@gmail.com>
2024-08-13 12:58:48 +0200

    Fix clip

--- drum_machine/src/App.tsx
+++ drum_machine/src/App.tsx
@@ -52,15 +52,23 @@ const audioClips: AudioClip[] = [
 ];
  
 function App() {
-  const [count, setCount] = useState(0);
+
+  const playAudio = (e: React.KeyboardEvent<HTMLDivElement) => {
+    const clip = audioClips.find((clip) => clip.keyTrigger === e.key.toUpperCase());
+    if (!clip) return; 
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement)?.play().catch(console.error);
+
+    document.getElementById("drum-" + clip.keyTrigger)?.focus();
+    document.getElementById("display")!.innerText = clip.description;
+};
 
   return (
     <>
-      <div className="container" id='drum-machine'>
+      <div className="container" id='drum-machine' onKeyDown={playAudio}>
         <h1>Drum Machine</h1> 
         <div className="whole-drum">
           {audioClips.map((clip) => (
-            <Drum audioClip={clip} key={audioClips.keyTrigger}/>
+            <Drum audioClip={clip} key={clip.keyTrigger}/>
           ))}
         </div>
       </div>
